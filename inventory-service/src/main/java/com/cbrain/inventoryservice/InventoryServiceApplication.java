703a77ffcd178229bc476c8ddaec507eec3eb730
DSP Chathuranga <dspchathuranga@gmail.com>
2023-02-08 00:04:52 +0530

    initial-inventory-service

---
+++ inventory-service/src/main/java/com/cbrain/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,33 @@
+package com.cbrain.inventoryservice;
+
+import com.cbrain.inventoryservice.model.Inventory;
+import com.cbrain.inventoryservice.repository.InventoryRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+//	@Bean
+//	public CommandLineRunner loadData(InventoryRepository inventoryRepository) {
+//		return args -> {
+//			Inventory inventory = new Inventory();
+//			inventory.setSkuCode("iPhone13pro128Black");
+//			inventory.setQuantity(100);
+//
+//			Inventory inventory1 = new Inventory();
+//			inventory1.setSkuCode("iPhone13proMax256Black");
+//			inventory1.setQuantity(0);
+//
+//			inventoryRepository.save(inventory);
+//			inventoryRepository.save(inventory1);
+//		};
+//	}
+
+}
