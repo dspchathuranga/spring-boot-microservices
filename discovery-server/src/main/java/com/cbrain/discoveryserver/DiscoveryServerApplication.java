f2e411852215f252d53aa91a80a8a8401f509b04
DSP Chathuranga <dspchathuranga@gmail.com>
2023-02-08 10:09:45 +0530

    Eureka implementation

--- discovery-server/src/main/java/com/cbrain/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/com/cbrain/discoveryserver/DiscoveryServerApplication.java
@@ -1,4 +1,4 @@
-package org.cbrain.discoveryserver;
+package com.cbrain.discoveryserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class DiscoveryServerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(DiscoveryServerApplication.class, args);
-    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+
 }
